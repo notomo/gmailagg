d54a4a9e60adeec7db6ebbba9ea160c031b89938
notomo <notomo.motono@gmail.com>
2023-08-03 09:54:55 +0900

    Init terraform

---
+++ infra/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.76.0"
+    }
+  }
+  backend "gcs" {
+    bucket = "gmailagg-tfstate"
+  }
+}
+
+provider "google" {
+  project = "gmailagg"
+  region  = "us-west1"
+  zone    = "us-west1-b"
+}
