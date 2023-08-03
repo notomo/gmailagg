bb54d59f0b74fde67ca52a1aac77cb2b0dafdd65
notomo <notomo.motono@gmail.com>
2023-08-03 20:47:47 +0900

    Fix terraform version

--- infra/main.tf
+++ infra/main.tf
@@ -1,8 +1,9 @@
 terraform {
+  required_version = "~> 1.5.0"
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.76.0"
+      version = "~> 4.76.0"
     }
   }
   backend "gcs" {
