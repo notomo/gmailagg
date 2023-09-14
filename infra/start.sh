b68e66c72e72007d0fa21022fcdbd812f2d6d864
notomo <notomo.motono@gmail.com>
2023-09-14 21:21:09 +0900

    Improve deleting tailscale node

--- infra/start.sh
+++ infra/start.sh
@@ -3,3 +3,4 @@ set -eu
 /tailscaled --tun=userspace-networking --outbound-http-proxy-listen=localhost:1055 &
 /tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=gmailagg-$1
 HTTP_PROXY=http://localhost:1055/ http_proxy=http://localhost:1055/ /gmailagg --config=gs://gmailagg-config/production.json $@
+/tailscale logout
