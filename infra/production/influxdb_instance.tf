850342194691d407c95dc431c84dd92b4f33525f
notomo <notomo.motono@gmail.com>
2024-07-04 07:45:02 +0900

    Update cos image

--- infra/production/influxdb_instance.tf
+++ infra/production/influxdb_instance.tf
@@ -31,7 +31,7 @@ module "influxdb_container" {
   source  = "terraform-google-modules/container-vm/google"
   version = "~> 3.1"
 
-  cos_image_name = "cos-beta-109-17800-0-13"
+  cos_image_name = "cos-113-18244-85-49"
 
   container = {
     image = "marketplace.gcr.io/google/influxdb2:2.7"
